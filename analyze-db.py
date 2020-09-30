13a90b0ab7566dbefca8febf13e5c618a1f95a95
Nicolas Jeanselme <41996571+njeanselme@users.noreply.github.com>
2020-09-30 19:04:25 +0200

    Update analyze-db.py

--- analyze-db.py
+++ analyze-db.py
@@ -51,20 +51,34 @@ def generate_report(providers,conn):
 	for provider in providers:
 		ioc_sets[provider] = getIOCs(conn,'provider = "{}"'.format(provider))
 	
+	#['AISCOMM' 'CrowdStrike' 'Cyber threat coalition' 'EmergingThreats' 'FarsightSecurity' 'Fortinet' 'IID' 'Palo Alto' 'SURBL']
+
+	
 	if 'Fortinet' in providers:
 		display_sets['Fortinet'] = ioc_sets['Fortinet']
 	if 'Palo Alto' in providers:
 		display_sets['Palo Alto']= ioc_sets['Palo Alto']
 	if 'FarsightSecurity' in providers:
 		display_sets['FarsightSecurity']=ioc_sets['FarsightSecurity']
-	
-	display_sets['Infoblox'] = set()
-	for provider in set(ioc_sets).difference(set(display_sets)):
-		display_sets['Infoblox'].update(ioc_sets[provider])
+	#if 'AISCOMM' in providers:
+	#	display_sets['AISCOMM'] = ioc_sets['AISCOMM']
+	#if 'CrowdStrike' in providers:
+	#	display_sets['CrowdStrike'] = ioc_sets['CrowdStrike']
+	#if 'SURBL' in providers:
+	#	display_sets['SURBL'] = ioc_sets['SURBL']
+	#if 'Cyber threat coalition' in providers:
+	#	display_sets['Cyber threat coalition'] = ioc_sets['Cyber threat coalition']
+	if 'IID' in providers:
+		display_sets['Infoblox'] = ioc_sets['IID']
+
+	#display_sets['Infoblox'] = set()
+	#for provider in set(ioc_sets).difference(set(display_sets)):
+	#	display_sets['Infoblox'].update(ioc_sets[provider])
 	
 	plt.figure(figsize=(4,4))
 	v= venn(display_sets)
-	plt.title('Vendor IOCs overlap on active threats during last {}days'.format(age_IOCs_inactive_for_days))
+	#plt.title('Vendor IOCs overlap on active threats during last {}days'.format(age_IOCs_inactive_for_days))
+	plt.title('Vendor IOCs overlap on active threats')
 	plt.savefig('IOCs_overlap.png')
 
 #########################################################			
