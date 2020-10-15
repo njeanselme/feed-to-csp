b05c69f52930df359ba07d7fa91e02eb1f121804
Nicolas Jeanselme <41996571+njeanselme@users.noreply.github.com>
2020-10-15 14:32:45 +0200

    Update and rename age-db.py to 2-age-db.py

--- 2-age-db.py
+++ 2-age-db.py
@@ -10,8 +10,7 @@ with open("config.yml", "r") as ymlfile:
 
 #########################################################	
 
-logging.basicConfig(handlers = [logging.FileHandler('log/age-db.log'), logging.StreamHandler()],level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-#logging.basicConfig(handlers = [logging.FileHandler('log/age-db.log'), logging.StreamHandler()], level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(handlers = [logging.FileHandler('log/age-db.log'), logging.StreamHandler()],level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 age_IOCs_inactive_for_days = cfg['age_IOCs_inactive_for_days']
 
@@ -20,26 +19,20 @@ deleteOlderThan= (now - timedelta(days=age_IOCs_inactive_for_days)).isoformat(ti
 
 #########################################################
 
-def initSQLlite():
+def ageDB():
 	conn = sqlite3.connect('IOCs.db')
 	logging.info('Opened database successfully')
-
-	return conn
-
-#########################################################
-
-def ageDB(conn,deleteOlderThan):
 	cur = conn.cursor()
 	cur.execute('select * from iocs where last_seen <"'+deleteOlderThan+'"')
 	rows = cur.fetchall()
-	for row in rows:
-		logging.debug(row)
+	if len(rows) > 0:
+		for row in rows:
+			logging.debug(row)
 		
-	cur.execute('select * from iocs where last_seen <"'+deleteOlderThan+'"')
-	conn.commit()
+		cur.execute('select * from iocs where last_seen <"'+deleteOlderThan+'"')
+		conn.commit()
 	logging.info('Aging database successfully - removed {}'.format(len(rows)))
 
 #########################################################
 
-conn = initSQLlite()
-ageDB(conn,deleteOlderThan)
+ageDB()
