e4ff50207c74bfcb5a53e3c67ed3ee5cf7553e27
Nicolas Jeanselme <41996571+njeanselme@users.noreply.github.com>
2020-10-15 14:38:46 +0200

    Add files via upload

---
+++ d-farsight-time-gained-backtest.py
@@ -0,0 +1,103 @@
+import json
+import re
+import math
+import logging
+import time
+import sqlite3
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from venn import venn
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import yaml
+import whois
+from datetime import datetime
+
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+#########################################################	
+
+logging.basicConfig(handlers = [logging.FileHandler('log/farsight-time-gained-backtest.log'), logging.StreamHandler()],level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+#########################################################	
+#['AISCOMM' 'CrowdStrike' 'Cyber threat coalition' 'EmergingThreats' 'FarsightSecurity' 'Fortinet' 'IID' 'Palo Alto' 'SURBL']
+		
+def generate_report():
+
+	conn = sqlite3.connect('IOCs.db')
+	logging.info('Opened database successfully')
+
+	query = conn.execute('''SELECT a.ioc, a.provider, a.first_seen
+		FROM iocs a
+		JOIN (SELECT ioc, COUNT(*)
+		FROM iocs 
+		GROUP BY ioc
+		HAVING count(*) > 1 and provider='FarsightSecurity') b
+		ON a.ioc = b.ioc WHERE a.ioc IN (SELECT threat_indicator FROM nod_matches)
+		ORDER BY a.ioc,a.first_seen''')
+	
+	cols = [column[0] for column in query.description]
+	df= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+	
+	logging.info(df.ioc.unique())
+	
+	# set farsight first seen as baseline - farsight_first_seen
+	df2= df.loc[df['provider'] == 'FarsightSecurity', ('ioc', 'first_seen')]
+	df2.rename(columns = {'first_seen':'farsight_first_seen'}, inplace = True)
+	df3 = pd.merge(df,df2,on='ioc',how='outer')
+
+	df3.drop(df3[df3.provider == 'FarsightSecurity'].index, inplace=True) #to remove from graph
+	#df3.drop(df3[df3.provider == 'CrowdStrike'].index, inplace=True) #to remove from graph
+	#df3.drop(df3[df3.provider == 'Cyber threat coalition'].index, inplace=True) #to remove from graph
+	#df3.drop(df3[df3.provider == 'SURBL'].index, inplace=True) #to remove from graph
+	#df3.drop(df3[df3.provider == 'EmergingThreats'].index, inplace=True) #to remove from graph
+	#df3.drop(df3[df3.provider == 'Palo Alto'].index, inplace=True) #to remove from graph
+	#df3.drop(df3[df3.provider == 'IID'].index, inplace=True) #to remove from graph
+	#df3.drop(df3[df3.provider == 'Fortinet'].index, inplace=True) #to remove from graph
+	
+	
+	df3['time_delta_human'] = pd.to_datetime(df3['first_seen']) - pd.to_datetime(df3['farsight_first_seen'])
+	df3['time_delta'] 	   = (pd.to_datetime(df3['first_seen']) - pd.to_datetime(df3['farsight_first_seen'])).dt.total_seconds()/3600
+	average_time_gain = round(df3['time_delta'].mean(),1)
+	
+	# Create the data
+	x = df3.time_delta
+	g = df3.provider
+	df = pd.DataFrame(dict(x = x, g = g))
+
+	# Initialize the FacetGrid object
+	pal = sns.cubehelix_palette(10, rot=-.25, light=.7)
+	g = sns.FacetGrid(df, row="g", hue="g", aspect=15, height=1, palette=pal)
+	
+	g.map(sns.kdeplot, "x", bw_adjust=.15, clip_on=False, fill=True, common_norm=True, alpha=1, linewidth=1.5)
+
+	# Define and use a simple function to label the plot in axes coordinates
+	def label(x, color, label):
+		label = label + '\n{}h\n{} IOCs'.format(
+			round(df3[df3.provider == label].time_delta.mean(),1),
+			len(df3[df3.provider == label].index))
+		ax = plt.gca()
+		ax.text(0, .2, label, fontweight="bold", color=color, ha="left", va="center", transform=ax.transAxes)
+	
+	g.map(label, "x")
+	plt.xlabel("Delay to Farsight NOD in hours")
+
+	# Set the subplots to overlap
+	g.fig.subplots_adjust(hspace=0.5)
+
+	# Remove axes details that don't play well with overlap
+	g.set_titles('')
+	g.set(yticks=[])
+	g.despine(bottom=True, left=True)
+
+	plt.title('Average time gain: {}hours'.format(average_time_gain))
+	plt.savefig('backtest/Farsight_timeline.png')
+	plt.close(g.fig)
+	logging.info('Generated backtest/Farsight_timeline.png successfully')	
+	
+#########################################################			
+
+generate_report()
---
+++ e-notable-timeline-backtest.py
@@ -0,0 +1,97 @@
+import json
+import re
+import math
+import logging
+import time
+import sqlite3
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from venn import venn
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import yaml
+import whois
+from datetime import datetime
+
+with open("config.yml", "r") as ymlfile:
+    cfg = yaml.safe_load(ymlfile)
+
+#########################################################	
+
+logging.basicConfig(handlers = [logging.FileHandler('log/notable-timeline-backtest.log'), logging.StreamHandler()],level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+age_IOCs_inactive_for_days = cfg['age_IOCs_inactive_for_days']
+		
+def get_iocs():
+
+	conn = sqlite3.connect('IOCs.db')
+	logging.info('Opened database successfully')
+
+	query = conn.execute('''SELECT a.ioc, a.provider, a.first_seen
+	FROM iocs a
+	JOIN (select ioc, provider,
+	COUNT(CASE WHEN provider = 'Palo Alto'  THEN 1 END) AS pan,
+	COUNT(CASE WHEN provider = 'Fortinet' THEN 1 END) AS fortinet,
+	COUNT(CASE WHEN provider = 'IID' THEN 1 END) AS iid,
+	COUNT(CASE WHEN provider = 'FarsightSecurity'   THEN 1 END) AS nod,
+	COUNT(*)
+	from iocs group by ioc
+	having count(*) > 1 and nod=1) b
+	ON a.ioc = b.ioc WHERE a.ioc IN (SELECT threat_indicator FROM nod_matches)
+	ORDER BY a.ioc,a.first_seen''')
+
+	cols = [column[0] for column in query.description]
+	df= pd.DataFrame.from_records(data = query.fetchall(), columns = cols)
+
+	for ioc in df.ioc.unique():		
+		logging.info(df.loc[df.ioc == ioc])
+		dates = df.loc[df.ioc == ioc].first_seen
+		names = df.loc[df.ioc == ioc].provider + " " + df.loc[df.ioc == ioc].first_seen
+		generate_report(dates,names,ioc)
+
+def generate_report(dates,names,ioc):
+	#Convert date strings (e.g. 2014-10-18) to datetime
+	dates = [datetime.strptime(d, "%Y-%m-%dT%H:%M:%S") for d in dates]
+	hour_interval = int(round((max(dates)-min(dates)).total_seconds()/3600/10,0))
+	if hour_interval==0:
+		return 0
+		
+	# Choose some nice levels
+	levels = np.tile([-5, 5, -3, 3, -1, 1],
+	int(np.ceil(len(dates)/6)))[:len(dates)]
+
+	# Create figure and plot a stem plot with the date
+	fig, ax = plt.subplots(figsize=(8.8, 4), constrained_layout=True)
+	ax.set(title="Matplotlib release dates")
+
+	ax.vlines(dates, 0, levels, color="tab:red")  # The vertical stems.
+	ax.plot(dates, np.zeros_like(dates), "-o",
+		color="k", markerfacecolor="w")  # Baseline and markers on it.
+
+	# annotate lines
+	for d, l, r in zip(dates, levels, names):
+		ax.annotate(r, xy=(d, l),
+			xytext=(-3, np.sign(l)*3), textcoords="offset points",
+			horizontalalignment="right",
+			verticalalignment="bottom" if l > 0 else "top")
+
+	# format xaxis with 10 hours intervals
+	ax.get_xaxis().set_major_locator(mdates.HourLocator(interval=hour_interval))
+	ax.get_xaxis().set_major_formatter(mdates.DateFormatter("%Y-%m-%d %H:%M"))
+	plt.setp(ax.get_xticklabels(), rotation=30, ha="right")
+
+	# remove y axis and spines
+	ax.get_yaxis().set_visible(False)
+	for spine in ["left", "top", "right"]:
+		ax.spines[spine].set_visible(False)
+
+	ax.margins(y=0.1)
+	plt.title('Detection timeline for ioc {}'.format(ioc))
+	plt.savefig('backtest/{}_timeline.png'.format(ioc))
+	plt.close(fig)
+	logging.info('Generated backtest/{}_timeline.png successfully'.format(ioc))	
+	
+#########################################################			
+
+get_iocs()
