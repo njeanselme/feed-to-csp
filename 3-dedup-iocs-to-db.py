066662524f062c313c0728d52cffcbe71c6abd20
Nicolas Jeanselme <41996571+njeanselme@users.noreply.github.com>
2020-10-29 18:57:05 +0100

    Update 3-dedup-iocs-to-db.py
    rewritten completely the code for generic list generation including optimal overlap management (ioc known by multiple vendors)

--- 3-dedup-iocs-to-db.py
+++ 3-dedup-iocs-to-db.py
@@ -48,49 +48,73 @@ def getIOCs(conn,filter):
 	cur.execute('select ioc from iocs where {}'.format(filter))
 	iocs = set(numpy.unique(cur.fetchall()))
 	return iocs
+	
+#########################################################
+	
+def generate_logics(n_sets):
+    """Generate intersection identifiers in binary (0010 etc)"""
+    for i in range(1, 2**n_sets):
+        yield bin(i)[2:].zfill(n_sets)
 		
 #########################################################
-		
+
+def generate_petal_labels(datasets):
+    datasets = list(datasets)
+    n_sets = len(datasets)
+    dataset_union = set.union(*datasets)
+    universe_size = len(dataset_union)
+    petal_labels = {}
+    petal_sets = {}
+    for logic in generate_logics(n_sets):
+        included_sets = [
+            datasets[i] for i in range(n_sets) if logic[i] == "1"
+        ]
+        excluded_sets = [
+            datasets[i] for i in range(n_sets) if logic[i] == "0"
+        ]
+        petal_set = (
+            (dataset_union & set.intersection(*included_sets)) -
+            set.union(set(), *excluded_sets)
+        )
+        petal_labels[logic] = logic
+        petal_sets[logic]=petal_set
+
+    return petal_sets
+
+#########################################################
+
 def dedupIOCtoDB(providers,conn):
+
 	ioc_sets = {}
-	temporary_sets = {}
 	deduplicated_sets = {}
 	
 	for provider in providers:
 		ioc_sets[provider] = getIOCs(conn,'provider = "{}"'.format(provider))
+
+	deduplicated_sets = generate_petal_labels(ioc_sets.values())
+	deduplicated_sets_keys = list(deduplicated_sets.keys())
 	
-	if 'Fortinet' in providers:
-		temporary_sets['Fortinet'] = ioc_sets['Fortinet']
-	if 'Palo Alto' in providers:
-		temporary_sets['Palo Alto']= ioc_sets['Palo Alto']
-	if 'Cyber threat coalition' in providers:
-		temporary_sets['Cyber threat coalition'] = ioc_sets['Cyber threat coalition']
-
-	temporary_sets['Infoblox'] = set()
-	for provider in set(ioc_sets).difference(set(temporary_sets)):
-		temporary_sets['Infoblox'].update(ioc_sets[provider])
+	ioc_sets_keys = list(ioc_sets.keys())
+	
+	#exclude AISCOMM, EmergingThreats, FarsightSecurity,  IID, SURBL
+	for provider in ioc_sets_keys:
+		if provider == 'AISCOMM' or provider == 'EmergingThreats' or provider == 'FarsightSecurity' or provider == 'IID' or provider == 'SURBL':
+			for binary_id in deduplicated_sets_keys:
+				if binary_id[ioc_sets_keys.index(provider)] == '1' and binary_id in deduplicated_sets.keys():
+					deduplicated_sets.pop(binary_id)
+	
+	for binary_id in deduplicated_sets:
+		provider = []
+		for binary_bin in range(0,len(binary_id)):
+			if binary_id[binary_bin] == '1':
+				provider.append(ioc_sets_keys[binary_bin])
+		first_provider = provider[0]
+		provider_display = ' and '.join(provider)
 		
-	if 'Fortinet' in providers and 'Palo Alto' in providers:
-		deduplicated_sets['Fortinet'] = ioc_sets['Fortinet'].difference(temporary_sets['Infoblox'].union(ioc_sets['Palo Alto']))
-		deduplicated_sets['Palo Alto'] = ioc_sets['Palo Alto'].difference(temporary_sets['Infoblox'].union(ioc_sets['Fortinet']))
-		deduplicated_sets['Palo Alto and Fortinet'] = ioc_sets['Fortinet'].intersection(ioc_sets['Palo Alto'])
-	elif 'Fortinet' in providers:
-		deduplicated_sets['Fortinet'] = ioc_sets['Fortinet'].difference(temporary_sets['Infoblox'])
-	elif 'Palo Alto' in providers:
-		deduplicated_sets['Palo Alto'] = ioc_sets['Palo Alto'].difference(temporary_sets['Infoblox'])
-
-	if 'Cyber threat coalition' in providers and 'Fortinet' in providers and 'Palo Alto' in providers:
-		deduplicated_sets['Cyber threat coalition'] = ioc_sets['Cyber threat coalition'].difference(temporary_sets['Infoblox'].union(ioc_sets['Palo Alto']).union(ioc_sets['Fortinet']))
-
-	for provider in deduplicated_sets:
-		if provider == 'Palo Alto and Fortinet':
-			providersearch = 'Fortinet'
-		else:
-			providersearch = provider
-		for ioc in deduplicated_sets[provider]:
-			cur = conn.cursor()
-			cur.execute('insert into deduplicated_iocs (ioc,provider,description) select "'+ioc+'","'+provider+'",description from iocs where id="'+ioc+providersearch+'"')
-			conn.commit()
+		cur = conn.cursor()
+		for ioc in deduplicated_sets[binary_id]:
+			cur.execute('insert into deduplicated_iocs (ioc,provider,description) select "'+ioc+'","'+provider_display+'",description from iocs where id="'+ioc+first_provider+'"')
+		conn.commit()
 
 	logging.info('IOC deduplication successful')
 		
